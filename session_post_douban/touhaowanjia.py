a0821e3d5ccca374e302f196925e700a86440f21
1400720231 <937886362@qq.com>
2018-04-07 15:52:59 +0800

    登陆豆瓣看《头号玩家》评论

--- session_post_douban/touhaowanjia.py
+++ session_post_douban/touhaowanjia.py
@@ -1,28 +1,55 @@
 # coding:utf-8
 import requests
 from bs4 import BeautifulSoup
-
-
-url = 'https://github.com/session' 
+from urllib.request import urlretrieve
 s = requests.session()
-res = s.get(url)
-html = BeautifulSoup(res.text, 'html.parser')
-token = html.find('input',{'name':'authenticity_token'})['value']
-login_url = 'https://github.com/session'
+login_url = 'https://accounts.douban.com/login'
+get_url = 'https://movie.douban.com/subject/4920389/comments?start=480&limit=20&sort=new_score&status=P&percent_type=' 
 
-data = {
-		'authenticity_token':token,
-		'commit':'Sign+in',
-		'login':'1400720231',
-		'password':'250onion????',
-		'utf8':'✓'
+res =s.get(login_url)
+html = BeautifulSoup(res.text,'html.parser')
+# 验证码图片地址
+href_tagert = html.find_all('img',{'id':'captcha_image'})
+
+#如果有验证码存在就用构建一个有验证码信息的data,没有就构建另一个
+if len(href_tagert)==0:
+	data = {
+	'source':'movie',
+	'redir':'https://movie.douban.com/',
+	'form_email': '18778331181',
+	'form_password': '250onion????',
+	'login':'登录'}
+else:
+	solution_href = href_tagert[0]['src']
+	urlretrieve(solution_href)  # 下载到本地默认的图片下载保存地址了
+	solution = input('输入你的验证码：')
+	captcha_id = html.find_all('input',{'name':'captcha-id'})[0]['value']
+	data = {
+	'captcha-id':captcha_id,
+	'captcha-solution':solution,  # 验证码的值
+	'source':'movie',
+	'redir':'https://movie.douban.com/',
+	'form_email': '你的帐号',
+	'form_password': '你的密码',
+	'login':'登录'
 	}
-
-res2 = s.post(login_url, data=data)
-a = BeautifulSoup(res2.text,'html.parser')
-for i in a.find_all('span',{'class':'text-bold repo'}):
-	print(i.string)
-res3 = s.get('https://github.com/1400720231/Django-Projects')
-html = BeautifulSoup(res3.text, 'html.parser')
-a = html.find_all('td', {"class":'content'})
-print(a)
+# 图片链接
+# print(href)
+print(captcha_id)
+print(solution)
+# source: movie
+# redir: https://movie.douban.com/
+# form_email: xxxxxxxxx
+# form_password: 250onion????
+# captcha-solution: prison
+# captcha-id: GxO4oH5kxjtVOQGbI913iZpE:en
+# login: 登录
+headers = {
+'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+}
+res = s.post(login_url,headers=headers,data=data)
+res = s.get(get_url)
+html = BeautifulSoup(res.text, 'html.parser')
+comments = html.find_all('div', {'class':'comment-item'})
+for i in comments:
+	print(i.find('p').string)
